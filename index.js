9df24543c88461d9b38d7a941aa9175794a54c46
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-05-18 00:49:24 +0600

    completed initial mongoDB setup

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
 const app = express()
 const port = process.env.PORT || 5000
@@ -7,6 +8,39 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+
+// MongoDB
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vvq0dey.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const toyCollection = client.db('toyMarketplace').collection('toys')
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();toyMarketplace
+  }
+}
+run().catch(console.dir);
+
+
+// toyMarketplace
+// 0xtbCKq4fQYafZbe
 
 app.get('/', (req, res) => {
   res.send("Toys are playing")
