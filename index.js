31074b7c91a55c81e9240ebc750cea6c9ea4b1cc
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-05-20 16:02:18 +0600

    added delete and update

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ async function run() {
     await client.connect();
 
     const toysCollection = client.db("toyMarketplace").collection("toys");
+    const newToysCollection = client.db("toyMarketplace").collection("newToys")
 
     app.get("/toys", async (req, res) => {
       const cursor = toysCollection.find();
@@ -77,6 +78,68 @@ async function run() {
       const result = await toysCollection.find(query, options).toArray();
       res.send(result);
     });
+
+    app.get("/newToys", async (req, res) => {
+      const cursor = newToysCollection.find();
+      const result = await cursor.toArray();
+      // console.log(result)
+      res.send(result);
+    });
+
+    app.post('/newToys', async (req, res) => {
+      const newToy = req.body
+      console.log(newToy)
+      const result = await newToysCollection.insertOne(newToy)
+      res.send(result)
+    })
+
+    app.get("/newToys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const options = {
+        projection: {
+          photo: 1,
+          name: 1,
+          price: 1,
+          description: 1,
+          sellerName: 1,
+          sellerEmail: 1,
+          quantity: 1,
+          rating: 1,
+          subCategory: 1,
+        },
+      };
+
+      const result = await newToysCollection.find(query, options).toArray();
+      res.send(result);
+    });
+
+    app.patch('/newToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const updatedToy = req.body
+      console.log(updatedToy)
+
+      const updateDoc = {
+        $set: {
+          price: updatedToy.price,
+          quantity: updatedToy.quantity,
+          details: updatedToy.details,
+        },
+      }
+
+      const result = await newToysCollection.updateOne(query, updateDoc);
+      res.send(result)
+    })
+
+    app.delete('/newToys/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await newToysCollection.deleteOne(query);
+      console.log(result)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
