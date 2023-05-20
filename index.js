e2eb1d27dd7b86e33e6551f664aa0edb9f3de951
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-05-20 23:47:10 +0600

    loaded new data from mongoDB and pass data to client server by verifying email

--- index.js
+++ index.js
@@ -80,20 +80,48 @@ async function run() {
     });
 
     app.get("/newToys", async (req, res) => {
-      console.log(req.query.email)
+      // console.log(req.query.email)
       let query = {}
       if (req.query?.email) {
         query = { email: req.query.email }
       }
       const cursor = newToysCollection.find(query);
       const result = await cursor.toArray();
+      // console.log(result)
+      res.send(result);
+    });
+
+    app.get("/newToys/category/:category", async (req, res) => {
+      const category = req.params.category;
+      
+      let query = {}
+      if (req.query?.email) {
+        query = { email: req.query.email, subCategory: category }
+      }
+
+      console.log(query)
+
+      const options = {
+        projection: {
+          image: 1,
+          name: 1,
+          price: 1,
+          description: 1,
+          seller: 1,
+          quantity: 1,
+          rating: 1,
+          subCategory: 1,
+        },
+      };
+
+      const result = await newToysCollection.find(query, options).toArray();
       console.log(result)
       res.send(result);
     });
 
     app.post('/newToys', async (req, res) => {
       const newToy = req.body
-      console.log(newToy)
+      // console.log(newToy)
       const result = await newToysCollection.insertOne(newToy)
       // console.log(result)
       res.send(result)
@@ -125,7 +153,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const updatedToy = req.body
-      console.log(updatedToy)
+      // console.log(updatedToy)
 
       const updateDoc = {
         $set: {
@@ -143,7 +171,7 @@ async function run() {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) }
       const result = await newToysCollection.deleteOne(query);
-      console.log(result)
+      // console.log(result)
       res.send(result)
     })
 
